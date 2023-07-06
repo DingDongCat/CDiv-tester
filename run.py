a45e12cce70d8039d1eb7c549b6ce73ef09ded60
phoenix <1120202925@bit.edu.cn>
2023-07-06 06:10:10 +0000

    auto update

---
+++ run.py
@@ -0,0 +1,7 @@
+import os
+os.system('''
+sshpass -p raspi_cg@2021 scp -P 2000 -r /home/coder/CDiv-tester root@127.0.0.1:~\n
+sshpass -p raspi_cg@2021 ssh root@127.0.0.1 -p 2000 "python /root/CDiv-tester/backend_tester_arm/pi_run.py"\n
+sshpass -p raspi_cg@2021 scp -P 2000 -r root@127.0.0.1:/root/CDiv-tester /home/coder\n
+python3 auto_git.py
+''')
