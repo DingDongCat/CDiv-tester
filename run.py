c62bfe52b93865647ced7ca7b352b7587ca1f96d
phoenix <1120202925@bit.edu.cn>
2023-07-06 06:13:42 +0000

    auto update

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 os.system('''
+pip install gitpython
 sshpass -p raspi_cg@2021 scp -P 2000 -r /home/coder/CDiv-tester root@127.0.0.1:~\n
 sshpass -p raspi_cg@2021 ssh root@127.0.0.1 -p 2000 "python /root/CDiv-tester/backend_tester_arm/pi_run.py"\n
 sshpass -p raspi_cg@2021 scp -P 2000 -r root@127.0.0.1:/root/CDiv-tester /home/coder\n
